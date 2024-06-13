b7146dda48a7c128891811ca7bebcf9f576ae689
barabane <barabayne@gmail.com>
2024-06-13 07:34:15 +0500

    Add db pooling

--- database/db.py
+++ database/db.py
@@ -2,6 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from settings import DB_URL
 
-engine = create_async_engine(DB_URL)
+engine = create_async_engine(
+    DB_URL, pool_size=30, max_overflow=3, pool_recycle=3600, pool_timeout=5000)
 session_maker = sessionmaker(
     bind=engine, class_=AsyncSession, expire_on_commit=False)
